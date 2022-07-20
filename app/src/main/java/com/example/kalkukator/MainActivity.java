e0d423f478bc37747d0707163fc6102c82e68434
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-07-20 21:49:35 +0700

    feat : create luas persegi panjang

--- app/src/main/java/com/example/kalkukator/MainActivity.java
+++ app/src/main/java/com/example/kalkukator/MainActivity.java
@@ -13,32 +13,62 @@ import com.example.kalkukator.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    EditText p,l;
+    Button hitung;
+    TextView hasil;
 
     private AppBarConfiguration appBarConfiguration;
-private ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        p = (EditText)findViewById(R.id.panjang);
+        l = (EditText)findViewById(R.id.lebar);
+        hitung = (Button) findViewById(R.id.button);
+        hasil = (TextView)findViewById(R.id.txt_hasil);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        hitung.setOnClickListener(new View.OnClickListener(){
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                //jika tidak mengisi panjang dan lebar sekaligus maka akan tampil notifikasi
+                if(p.length()==0 && l.length()==0){
+                    Toast.makeText(getApplication(),"Panjang dan lebar tidak boleh Kosong",Toast.LENGTH_LONG).show();
+                }
+                //notifikasi ini akan muncul jika panjangnya tidak diisi
+                else if (p.length()==0){
+                    Toast.makeText(getApplication(),"Panjang tidak boleh kososng", Toast.LENGTH_LONG).show();
+                }
+                //notifikasi jika lebar tidak diisi
+                else if (l.length()==0){
+                    Toast.makeText(getApplication(),"Lebar tidak boleh kosong",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    String isipanjang = p.getText().toString();
+                    String isilebar = l.getText().toString();
+                    int p = Integer.parseInt(isipanjang);
+                    int l = Integer.parseInt(isilebar);
+                    int hs = LuasPersegiPanjang(p,l);
+                    String output = String.valueOf(hs);
+                    hasil.setText(output.toString());
+                }
             }
         });
     }
+    public int LuasPersegiPanjang(int p, int l){
+        return p*l;
+    }
+
+
+
 @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
